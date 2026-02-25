43decb5a1cd7e56aa7b388a2b6cb08b18742f763
Anoop P <anoop171215@gmail.com>
2026-02-25 11:01:25 +0530

    Add Terraform configuration for local and random providers

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "local" {}
+
+provider "random" {}
+
+resource "random_pet" "name" {
+  length = 2
+}
+
+resource "local_file" "demo_file" {
+  content  = "Hello from HCP Terraform! Your random name is ${random_pet.name.id}"
+  filename = "${path.module}/demo.txt"
+}
