98d1992bae57611fe93ffba608eb9a79c556314b
Anoop P <anoop171215@gmail.com>
2026-02-26 13:53:41 +0530

    Fix formatting of content in demo_file resource

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "random_pet" "name" {
 }
 
 resource "local_file" "demo_file" {
-  content  = "Hello from HCP Terraform! Your random name is ${random_pet.name.id}
-I live in ${var.place}"
+  content  = "Hello from HCP Terraform! Your random name is ${random_pet.name.id} \n I live in ${var.place}"
   filename = "${path.module}/demo.txt"
 }
